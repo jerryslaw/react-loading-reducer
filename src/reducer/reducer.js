f87750f58711b5901ddf7b20c32313891e6b78d0
Jarosław Wlazło <jaroslaw.wlazlo@grandparade.co.uk>
2017-04-04 13:22:03 +0200

    fix - handle Immutable and non-immutable state

--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -1,5 +1,8 @@
 import curry from 'lodash/curry';
+import { Iterable } from 'immutable';
 import errorHandler from '../errorHandler';
+
+const getNoKeyError = keyName => new Error(`There is no ${keyName} key in your state branch. The new one will be set`);
 
 export default curry((keyName, loadingActions, notLoadingActions, reducer) => (state, action) => {
     const possibleError = errorHandler(keyName, loadingActions, notLoadingActions, reducer);
@@ -8,8 +11,29 @@ export default curry((keyName, loadingActions, notLoadingActions, reducer) => (s
         return;
     }
 
-    state = loadingActions.includes(action.type) ? state.set(keyName, true) : state;
-    state = notLoadingActions.includes(action.type) ? state.set(keyName, false) : state;
+    const includesLoadingAction = loadingActions.includes(action.type);
+    const includesNotLoadingAction = notLoadingActions.includes(action.type);
+
+    if (Iterable.isIterable(state)) {
+
+        if (!state.has(keyName)) {
+            console.error(getNoKeyError(keyName));
+        }
+
+        state = includesLoadingAction ? state.set(keyName, true) : state;
+        state = includesNotLoadingAction ? state.set(keyName, false) : state;
+    } else if (typeof(state) === 'object') {
+
+        if (!state.hasOwnProperty(keyName)) {
+            console.error(getNoKeyError(keyName));
+        }
+
+        state = includesLoadingAction ? state[keyName] = true : state;
+        state = includesNotLoadingAction ? state[keyName] = false : state;
+    } else {
+        console.error('State branch should be an instance of either Object or Immutable.Iterable');
+        return;
+    }
 
     return reducer(state, action);
 })
